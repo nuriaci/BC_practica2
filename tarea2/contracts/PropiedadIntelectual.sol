38d484fd6ee495aba2569c2a3939a3915a3af90d
nuriaci <nuria.ci@hotmail.es>
2024-11-19 01:49:13 +0100

    cambios

--- tarea2/contracts/PropiedadIntelectual.sol
+++ tarea2/contracts/PropiedadIntelectual.sol
@@ -4,22 +4,23 @@ pragma solidity ^0.8.10;
 import "./openzeppelin-contracts/token/ERC721/ERC721.sol";
 import "./openzeppelin-contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-
 //Un contrato - tipo ERC-721 que emite el NFT  (Gestion de NFTs)
 /*Recompensas en NFT:  el sistema puede emitir un NFT como certificado digital cada vez que un usuario registre un archivo, permitiendo la tokenización de su propiedad intelectual.*/
 contract PropiedadIntelectualNFT  is ERC721URIStorage {
-    uint256 private _tokenIdCounter = 0; // Variable -> cuenta
+    uint256 private _tokenIdCounter; // Variable -> cuenta
     
     // Almacena el permiso de acceso para cada tokenId
     mapping(uint256 => mapping(address => bool)) private _accessList;
     // Almacena la expiración de las licencias temporales
     mapping(uint256 => mapping(address => uint)) private _licenciasTemporales;
 
-    constructor() ERC721("PropiedadIntelectualNFT", "PI_NFT") {}
-
+    constructor() ERC721("PropiedadIntelectualNFT", "PI_NFT") {
+        _tokenIdCounter = 0;
+    }
+ 
     // Función para emitir un nuevo NFT y asignarlo al propietario
     function emitirNFT(address propietario, string memory uri) public returns (uint256) {
-       _tokenIdCounter++; // // Incrementa el contador para obtener un nuevo ID de token
+        _tokenIdCounter++; // // Incrementa el contador para obtener un nuevo ID de token
         uint256 nuevoTokenId = _tokenIdCounter; // Obtiene el valor actual del contador como nuevo ID
         _safeMint(propietario, nuevoTokenId);//Crea un NFT y lo asigna al propietario
         _setTokenURI(nuevoTokenId, uri);//Asigna un URI (identificador de recursos) al token
@@ -33,6 +34,9 @@ contract PropiedadIntelectualNFT  is ERC721URIStorage {
 
      /*Comprueba el acceso: Verifica si un usuario tiene acceso al NFT*/
     function comprobarAcceso(uint256 tokenId, address usuario) public view returns (bool) {
+        if (ownerOf(tokenId) == usuario) {//El propietario siempre tiene acceso
+        return true;
+        }
         return _accessList[tokenId][usuario];
     }
 
@@ -101,6 +105,7 @@ contract PropiedadIntelectual{
     event registroRealizado(address propietario, string hash_ipfs, string titulo, uint fecha,uint256 tokenId);
     event TransferenciaPropiedad(address indexed antiguoPropietario, address indexed nuevoPropietario, uint256 tokenId, uint fecha);
     event DisputaRegistrada(address indexed reportante, address indexed propietario, uint256 tokenId, string motivo, uint fecha);
+    event Debug(address message);
 
     constructor(address nftAddress) {
         nftContract = PropiedadIntelectualNFT(nftAddress); // Inicializa el contrato 
@@ -115,9 +120,8 @@ contract PropiedadIntelectual{
         Archivo memory nuevoArchivo = Archivo(titulo,descripcion,hash_ipfs,block.timestamp); //Se crea un nuevo archivo
         archivos[msg.sender].push(nuevoArchivo);
         string memory uri = string(abi.encodePacked("ipfs://", hash_ipfs));//Se genera la uri
-
         uint256 tokenId = nftContract.emitirNFT(msg.sender,uri);//emitir certificado digital
-
+        
         emit registroRealizado(msg.sender,hash_ipfs,titulo,block.timestamp, tokenId);//Se emite el evento de registro realizado
     }
 
@@ -126,6 +130,9 @@ contract PropiedadIntelectual{
     function transferProperty(address nuevoPropietario, uint256 tokenId) public {
         require(nuevoPropietario != address(0), "El nuevo propietario no puede ser la direccion cero");
         require(nftContract.ownerOf(tokenId) == msg.sender, "Solo el propietario actual puede transferir la propiedad");
+
+        // Asegurar de que el contrato es aprobado para transferir el token
+        nftContract.approve(address(this), tokenId);
 
         // Agregar la transferencia al historial
         historialTransferencias[tokenId].push(Transferencia({
